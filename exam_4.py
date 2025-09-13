7db874b0929eef78df2b2381542c60b153adc51e
jaeng <jaengjaeng7@gmail.com>
2025-09-13 15:30:28 +0900

    gitignore testcode 추가

---
+++ exam_4.py
@@ -0,0 +1,16 @@
+# 코드 4-1 Requests의 예제 코드
+import requests
+
+# 가져올 웹 페이지의 URL
+url = "https://example.com"
+
+# HTTP GET 요청을 보내고 응답받기
+response = requests.get(url)
+
+#응답이 성공적으로 왔는지 확인
+if response.status_code == 200:
+  # HTML 문서 내용 출력
+  print(response.text)
+
+else:
+  print("웹 페이지를 불러오는데 실패했습니다.")
